b93990d9966c0a118bd6d1060ef32e58fb31ac56
Sowndar K <84360457+KSOWNDAR@users.noreply.github.com>
2022-01-17 07:51:43 +0530

    Create Linkedlist using recursion and iterative.java

---
+++ Linkedlist using recursion and iterative.java
@@ -0,0 +1,23 @@
+
+RECURSION:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode n=head.next;
+        ListNode nn=reverseList(n);
+        n.next=head;
+        head.next=null;
+        return nn;
+    }
+}
