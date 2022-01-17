b33550ce47ec80a05c4576bc3e4be061c2b96dbf
Sowndar K <84360457+KSOWNDAR@users.noreply.github.com>
2022-01-17 08:58:00 +0530

    Update Linkedlist using recursion.java

--- Linkedlist using recursion.java
+++ Linkedlist using recursion.java
@@ -21,3 +21,25 @@ class Solution {
         return nn;
     }
 }
+
+method 2:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode rn=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return rn;
+    }
+}
