4c37096a8aaea61fec06c3ea5751b2eddd3b6341
Sowndar K <84360457+KSOWNDAR@users.noreply.github.com>
2022-01-20 20:19:03 +0530

    Update Loop detection.java

--- Loop detection.java
+++ Loop detection.java
@@ -49,3 +49,32 @@ public class Solution {
         return false;
     }
 }
+
+
+Fast and Slow Method:
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow)
+                return true;
+        }
+        return false;
+    }
+}
