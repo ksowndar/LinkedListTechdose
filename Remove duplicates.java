704a690548078d7010da22114151595574ec8107
Sowndar K <84360457+KSOWNDAR@users.noreply.github.com>
2022-01-20 16:17:59 +0530

    Create Remove duplicates.java

---
+++ Remove duplicates.java
@@ -0,0 +1,45 @@
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null)
+            return head;
+        ListNode node=head;
+        while(node.next!=null)
+        {
+            if(node.val == node.next.val)
+            {
+                node.next=node.next.next;
+            }
+            else
+            {
+                node=node.next;
+            }
+        }
+        return head;
+    }
+}
